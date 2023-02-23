bdc98451a1806c12960005a66b6a9e5ff7f01a4b
unknown <brahimi.zakaria.abdessamed@gmail.com>
2023-02-23 11:05:07 +0100

    first Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 import TeachersList from './pages/teachersList/TeachersList';
 import TeacherProfile from './pages/teacherProfile/TeacherProfile';
 import Reservation from './pages/reservationForm/Reservation';
-import Login from './pages/login/login';
+import Login from './pages/login/Login';
 
 function App() {
 
