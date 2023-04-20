7da2d0298c2f143f0af044c0d4748458ca78923c
unknown <brahimi.zakaria.abdessamed@gmail.com>
2023-04-20 22:15:28 +0200

    production-ready configurations

--- backend/build.sh
+++ backend/build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --password aze1232000
+# python manage.py createsuperuser --username admin --password aze1232000
