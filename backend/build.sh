23c45b3015b50c9195b2423869d01fe94ce23c53
unknown <brahimi.zakaria.abdessamed@gmail.com>
2023-04-22 13:17:30 +0200

    without makemigrations command

--- backend/build.sh
+++ backend/build.sh
@@ -5,9 +5,18 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
 python manage.py makemigrations user_account 
-python manage.py showmigrations
 python manage.py migrate user_account
-python manage.py makemigrations
+
+python manage.py showmigrations
+
+python manage.py makemigrations sena_research
+python manage.py migrate sena_research
+
+python manage.py showmigrations
+
+python manage.py makemigrations 
 python manage.py migrate 
+
 python manage.py showmigrations
